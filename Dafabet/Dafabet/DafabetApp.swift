8978a4069f3ac70179caea6ff79d2c7dae422a7e
Dias <dias.atudinov@gmail.com>
2024-07-29 14:48:08 +0500

    first init

---
+++ Dafabet/Dafabet/DafabetApp.swift
@@ -0,0 +1,17 @@
+//
+//  DafabetApp.swift
+//  Dafabet
+//
+//  Created by Dias Atudinov on 29.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct DafabetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
