f8b95842c4c3eb5c0045e8daff0ada2cc7805890
Dias <dias.atudinov@gmail.com>
2024-07-29 17:17:57 +0500

    Loader and onboardings

--- Dafabet/Dafabet/First/Modules/Onboarding/ReOnboardingUIView.swift
+++ Dafabet/Dafabet/First/Modules/Onboarding/ReOnboardingUIView.swift
@@ -8,9 +8,130 @@
 import SwiftUI
 
 struct ReOnboardingUIView: View {
+    @State private var pageNum: Int = 1
+    @State private var showSheet = false
+    @AppStorage("signedUP") var signedUP: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if !signedUP {
+            ZStack {
+                Color.background
+                    .ignoresSafeArea()
+                VStack {
+                    Spacer()
+                    switch pageNum {
+                    case 1: Image("appScreen1")
+                            .resizable()
+                            .frame(height: 549)
+                            .ignoresSafeArea()
+                    case 2: Image("appScreen2")
+                            .resizable()
+                            .frame(height: 549)
+                            .ignoresSafeArea()
+                    case 3: Image("appScreen3")
+                            .resizable()
+                            .frame(height: 549)
+                            .ignoresSafeArea()
+                    default:
+                        Image("appScreen3")
+                            .resizable()
+                            .frame(height: 549)
+                            .ignoresSafeArea()
+                    }
+                    
+                    Button {
+                        if pageNum < 3 {
+                            pageNum += 1
+                        } else {
+                            signedUP = true
+                        }
+                    } label: {
+                        Text("Next")
+                            .foregroundColor(Color.white)
+                            .font(.headline)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                    }.background(Color.onboardingBtn)
+                        .cornerRadius(18)
+                        .padding(.horizontal, 20)
+                        .padding(.bottom)
+                    
+                }
+                VStack {
+                    HStack(spacing: 8) {
+                        Rectangle()
+                            .fill(pageNum == 1 ? Color.red : Color.gray.opacity(0.5))
+                            .frame(width: 35, height: 6)
+                            .cornerRadius(19)
+                        
+                        Rectangle()
+                            .fill(pageNum == 2 ? Color.red : Color.gray.opacity(0.5))
+                            .frame(width: 35, height: 6)
+                            .cornerRadius(19)
+                        
+                        Rectangle()
+                            .fill(pageNum > 2 ? Color.red : Color.gray.opacity(0.5))
+                            .frame(width: 35, height: 6)
+                            .cornerRadius(19)
+                    }
+                    .padding()
+                    switch pageNum {
+                    case 1:
+                        Text("All statistics in one \napplication")
+                            .font(.title)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .frame(height: 70)
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(.black)
+                            .padding(.bottom, 10)
+                    case 2:
+                        Text("Create trips and \ncelebrate achievements")
+                            .font(.title)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .frame(height: 70)
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(.black)
+                            .padding(.bottom, 10)
+                    case 3:
+                        Text("Adding information about \nthe downgrade")
+                            .font(.title)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .frame(height: 70)
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(.black)
+                            .padding(.bottom, 10)
+                    default:
+                        Text("Save information about \nyour favorite routes")
+                            .font(.title)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .frame(height: 70)
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(.white)
+                            .padding(.bottom, 10)
+                        
+                    }
+                    
+                }.padding(.bottom, UIScreen.main.bounds.height * 2/2.8)
+            }
+        
+            
+        } else {
+            TabUIView()
+        }
     }
+    
+//    func loadProfile() {
+//        if let data = UserDefaults.standard.data(forKey: "profile"),
+//           let decodedProfile = try? JSONDecoder().decode(ProfileModel.self, from: data) {
+//            profile = decodedProfile
+//        }
+//        
+//        if let profile = profile {
+//            signedUP = true
+//        } else {
+//            signedUP = false
+//        }
+//    }
 }
 
 #Preview {
--- Dafabet/Dafabet/LoaderView/LoaderUIView.swift
+++ Dafabet/Dafabet/LoaderView/LoaderUIView.swift
@@ -8,9 +8,136 @@
 import SwiftUI
 
 struct LoaderUIView: View {
+    @State private var progress: Double = 0.0
+    @State private var timer: Timer?
+    @State private var isLoadingView: Bool = true
+    @State private var isWeb: Bool = true
+    
+//    @State private var apiResponse: ApiResponse?
+//    private let apiService = ApiService()
+    @State private var errorMessage: String?
+    
+    @AppStorage("isRequested") var isRequested: Bool = false
+    @AppStorage("isBlock") var isBlock: Bool = true
+    @State var isDead: Bool = true
+
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if isLoadingView {
+            ZStack {
+                Color.background
+                    .ignoresSafeArea()
+                
+                VStack {
+                    Image("logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 235)
+                        .padding(.top, UIScreen.main.bounds.height / 4.8)
+                    Spacer()
+                    ZStack {
+                        
+                        ProgressView(value: progress, total: 100)
+                            .progressViewStyle(LinearProgressViewStyle())
+                            .accentColor(.red)
+                            .cornerRadius(15)
+                            .scaleEffect(y: 2.5, anchor: .center)
+                            .padding(.horizontal, 92)
+                            
+                    }
+                    HStack {
+                        Text("Loading")
+                            
+                            
+                        
+                        Text("\(Int(progress))%")
+                    }
+                    .font(.system(size: 17))
+                    .fontWeight(.semibold)
+                    .foregroundColor(.black)
+                    .padding(14)
+                    .padding(.bottom, 60)
+                }
+                .onAppear {
+                    startTimer()
+                }
+                .onDisappear {
+                    timer?.invalidate()
+                }
+                
+            }
+            
+        } else {
+            if true {
+                ReOnboardingUIView()
+                
+            } else {
+                UsOnboardingUIView()
+            }
+        }
     }
+    func startTimer() {
+        timer?.invalidate()
+        progress = 0
+        timer = Timer.scheduledTimer(withTimeInterval: 0.07, repeats: true) { timer in
+            if progress < 100 {
+                progress += 1
+            } else {
+                timer.invalidate()
+                isLoadingView.toggle()
+            }
+        }
+    }
+    
+//    func fetch() {
+//        apiService.fetchData { result in
+//            switch result {
+//            case .success(let data):
+//                self.apiResponse = data
+//            case .failure(let error):
+//                self.errorMessage = error.localizedDescription
+//            }
+//        }
+//    }
+//    
+//    private func checkWeb() -> Bool? {
+//        guard let response = apiResponse else {
+//            return nil
+//        }
+//        return response.sortable
+//    }
+    
+//    private func check_data() {
+//
+//        guard isRequested == false else {
+//
+//           // self.isFetched = true
+//
+//            return
+//        }
+//
+//        let networkService = NetworkService()
+//        let deviceData = DeviceInfo.collectData()
+//
+//        print(deviceData)
+//        networkService.sendRequest(endpoint: deviceData) { result in
+//
+//            isRequested = true
+//
+//            switch result {
+//
+//            case .success(let success):
+//
+//                self.isBlock = success
+//                //self.isFetched = true
+//
+//            case .failure(_):
+//
+//                self.isBlock = self.isDead
+//                //self.isFetched = true
+//            }
+//        }
+//    }
 }
 
 #Preview {
